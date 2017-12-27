57282d279356ca4423a0106f9d1cdf87165494e7
github username <github email address>
2017-12-26 20:56:56 -0500

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 class Game
 
-  attr_accessor :board, :player_1, :player_2
+  attr_accessor :board, :player_1, :player_2, :token
 
   WIN_COMBINATIONS = [
   [0,1,2],
@@ -10,11 +10,10 @@ class Game
   [1,4,7],
   [2,5,8],
   [0,3,6],
-  [2,4,6]
+  [6,4,2]
 ]
 
-  def initialize(player_1 = Players::Human.new("X"),
-    player_2 = Players::Human.new("O"), board = Board.new)
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @player_1 = player_1
     @player_2 = player_2
     @board = board
@@ -25,18 +24,53 @@ class Game
   end
 
   def over?
-    draw? || won?
+    board.full?
   end
 
   def won?
-    WIN_COMBINATIONS.detect do |combination|
-      if combination.all?{|i| @board.cells[i] == "X"} || combination.all?{|i| @board.cells[i] == "O"}
-        return combination
+    winning_array = []
+      WIN_COMBINATIONS.each do |win_combination|
+        position_1 = @board.cells[win_combination[0]]
+        position_2 = @board.cells[win_combination[1]]
+        position_3 = @board.cells[win_combination[2]]
+        if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+         winning_array = win_combination
+        end
       end
-    end
+      winning_array.empty? ? false : winning_array
   end
 
   def draw?
-    @board.full? && !won?
+    !won? && over?
   end
+
+  def winner
+    board.position(won?[0] + 1) if won?
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    user_input = self.current_player.move(@board)
+
+    if @board.valid_move?(user_input) == true
+      @board.update(user_input, self.current_player)
+      @board.display
+    else
+      turn
+    end
+  end
+
+  def play
+    while !draw? && !won?
+      sleep(1)
+      turn
+    end
+      if won?
+        puts "Congratulations #{winner}!"
+      elsif draw?
+        board.display
+        puts "Cat's Game!"
+     end
+  end
+
 end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,8 +1,9 @@
 module Players
   class Human < Player
-    def move(input)
+    def move(board)
       puts "Please choose a position between (1,9):"
       input = gets.strip
+      input
     end
   end
 end
