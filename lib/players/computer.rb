ab799917ddbede5d51c173756a983ec4578b5fa4
github username <github email address>
2017-12-29 14:47:57 -0500

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,19 +1,53 @@
 module Players
   class Computer < Player
 
-  def move(array)
-    valid_moves = []
-    i = 0
-    until i > 8
-      if array.valid_move?(i) == true
-         valid_moves << i
-      end
-         i += 1
-     end
-     valid_moves.sample.to_s
+    def for_the_win(board)
+            WIN_COMBINATIONS.each do |combo|
+                if board.cells[combo[0]] == board.cells[combo[1]] && board.cells[combo[2]] == " " && board.cells[combo[0]] == self.token
+                    @i = combo[2]
+                elsif board.cells[combo[1]] == board.cells[combo[2]] && board.cells[combo[0]] == " " && board.cells[combo[0]] == self.token
+                    @i = combo[0]
+                elsif board.cells[combo[0]] == board.cells[combo[2]] && board.cells[combo[1]] == " " && board.cells[combo[0]] == self.token
+                    @i = combo[1]
+                else
+                    nil
+                end
     end
   end
-end
+
+# #first move always middle (5), check if position is taken
+# def move(board)
+#   move = nil
+# #if middle square is empty, we want to take middle square
+#   if array[5].empty?
+#     return move = "5"
+#   elsif board.turn_count == 1
+#     return move = ["1", "3", "7", "9"].sample
+#   elsif board.turn_count == 2
+# #we want to find the first index that is not taken
+#     return move = ["1", "3", "7", "9"].detect {|i| !board.taken?(i)}
+# #turn_count 3, then start checking win_combinations and tracking and check if there
+# #are two squares
+# end
+#
+#
+#
+#
+#   def move(board)
+#     valid_moves = []
+#     i = 0
+#     until i > 8
+#       if board.valid_move?(i) == true
+#          valid_moves << i
+#       end
+#          i += 1
+#      end
+#      valid_moves.sample.to_s
+#     end
+#   end
+# end
+
+
 
 #   class Computer::Hard < Player
 #
